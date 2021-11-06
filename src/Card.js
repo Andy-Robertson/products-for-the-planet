dd0b474daa15897ee610b9798f9b0a53b883f2fd
AndyR <50073313+Andy-Robertson@users.noreply.github.com>
2021-11-06 17:06:38 +0000

    Added chakraUI

---
+++ src/Card.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Box, Flex, Stack, Grid, Wrap, AspectRatio } from "@chakra-ui/layout";
+
+const Card = () =>{
+
+
+
+    return (
+      <Box color="tomato" _hover={{ bg: "red.500", color: "white" }}>
+        Welcome to Box
+      </Box>
+    );
+}
+
+export default Card;
